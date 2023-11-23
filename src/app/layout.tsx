0c2a543d36afb3fce5ba0b3539e3abf40f26de55
Adrian Książek <adrian.ksiazek@sagiton.pl>
2023-11-23 17:43:23 +0100

    install packages and add workflow

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,37 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { Header } from "./components/header";
+import clxs from "clsx";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Adrian | Personal Portoflio',
-  description: 'Adrian is a frontend developer with over a year of experience',
+  title: "Adrian | Personal Portoflio",
+  description: "Adrian is a frontend developer with over a year of experience",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-gray-50 text-gray-950`}>
-        <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
-        <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
+      <Header />
+      <body className={`${inter.className} bg-gray-200 text-gray-950`}>
+        <div
+          className={clxs(
+            "absolute right-[11rem] top-[-6rem] -z-10 rounded-full",
+            "h-[31.25rem] w-[31.25rem] sm:w-[68.75rem]",
+            "bg-[#fbe2e3] blur-[10rem]",
+            "dark:bg-[#946263]",
+          )}
+        ></div>
+        <div
+          className={clxs(
+            "absolute -z-10 h-[31.25rem] w-[50rem] rounded-full",
+            "left-[-35rem] top-[-1rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]",
+            "bg-[#dbd7fb] blur-[10rem]",
+            "dark:bg-[#676394]",
+          )}
+        ></div>
         {children}
       </body>
     </html>
